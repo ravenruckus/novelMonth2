eb1f77c345afe45592ced1f260ca369f8584df2b
ravenruckus <gyorihome@hotmail.com>
2017-04-02 15:28:16 -0700

    Fix build errors.

--- src/shared/component/page/home.jsx
+++ src/shared/component/page/home.jsx
@@ -5,7 +5,7 @@ import Helmet from 'react-helmet'
 
 
 import { APP_NAME } from '../../config'
-import Test2 from '../test.jsx'
+import Test2 from '../test2.jsx'
 // import Intro2 from '../intro2.jsx'
 
 const HomePage = () =>
