2eb2780b612f169da32612b916e364804153c45b
ravenruckus <gyorihome@hotmail.com>
2017-04-03 07:48:27 -0700

    Add mobile viewport meta.

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -27,6 +27,7 @@ const renderApp = (location: string, plainPartialState: ?Object, routerContext: 
       <head>
         ${head.title}
         ${head.meta}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
         <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
       </head>
